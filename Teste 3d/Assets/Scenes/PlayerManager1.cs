9929f6769db395524343526bfc5f02481081af61
guilherme-222 <jesus.g@escolar.ifrn.edu.br>
2025-06-02 13:39:54 -0300

    0

---
+++ Teste 3d/Assets/Scenes/PlayerManager1.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Animator animator;
+    private MovimentoPlayer movimentoPlayer;
+    public float velocidadeDaAnimacao = 1;
+    
+    void Start()
+    {
+        movimentoPlayer = GetComponent<MovimentoPlayer>();
+    }
+   void Update()
+    {
+      animator.SetBool("Andando", movimentoPlayer.andando);
+      animator.speed = velocidadeDaAnimacao;
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+          animator.SetTrigger("Ataque");  
+      }
+    }
+}
